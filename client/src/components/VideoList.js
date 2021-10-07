d9ac233439cdf0a73918df419d40e3595b20e649
Luz Angelique Madrazo <luzm321@yahoo.com>
2021-10-07 18:28:43 -0500

    added 'Search Videos' feature to app; created VideoSearch component and refactored VideoRepository.Search() method to achieve search functionality

--- client/src/components/VideoList.js
+++ client/src/components/VideoList.js
@@ -1,9 +1,14 @@
 import React, { useEffect, useState } from "react";
 import Video from './Video';
-import { getAllVideosWithComments } from "../modules/videoManager";
+import VideoSearch from "./VideoSearch";
+import { getAllVideosWithComments} from "../modules/videoManager";
 
 const VideoList = () => {
+    // Array destructuring initializes variables and useState() hook returns an array of 2 things: the initial value of the state variable
+    // that is set by what passed to the hook and a function/method that updates that state/variable
   const [videos, setVideos] = useState([]);
+
+
 
   const getVideosWithComments = () => {
     getAllVideosWithComments().then(videos => setVideos(videos));
@@ -13,13 +18,23 @@ const VideoList = () => {
     getVideosWithComments();
   }, []);
 
+
   return (
-    <div className="container">
-      <div className="row justify-content-center">
-        {videos.map((video) => (
-          <Video video={video} key={video.id} />
-        ))}
-      </div>
+    <div>
+        <div className="container">
+            <div className="row justify-content-center">
+                <VideoSearch setVideos={setVideos}/>
+            </div>
+
+        </div>
+        <div className="container">
+            <div className="row justify-content-center">
+                {console.log(videos)}
+                {videos.map((video) => (
+                    <Video video={video} key={video.id} />
+                ))}
+            </div>
+        </div>
     </div>
   );
 };
---
+++ client/src/components/VideoSearch.js
@@ -0,0 +1,33 @@
+//Component's responsibility is to capture the text from the user. As the user types, the searchTerms state variable must immediately be 
+//updated in the parent component:
+
+
+import React from "react";
+import { searchVideos } from "../modules/videoManager";
+
+
+const VideoSearch = ({setVideos}) => {
+
+    return (
+        <div className="searchDiv">
+            <div className="searchBar">
+                <div className="search">Search Video:
+                    <img className="searchIcon" src="https://img.icons8.com/ultraviolet/48/4a90e2/search--v1.png" alt="search icon"/>
+                </div>
+                <input type="text"
+                    className="input--wide"
+                    onKeyUp={(event) => {
+                        // setVideos(event.target.value)
+                        console.log('is it triggering?', event.target.value);
+                        searchVideos(event.target.value).then((videoResults) => {
+                            console.log('video results', videoResults);
+                            setVideos(videoResults);
+                        });
+                    }}
+                    placeholder="Search for a video..." />
+            </div>
+        </div>
+    )
+};
+
+export default VideoSearch;
